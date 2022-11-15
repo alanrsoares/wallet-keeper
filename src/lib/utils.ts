2864871247f74abb933384bd0e968c791f960fa6
Alan Soares <alanrsoars@gmail.com>
2022-11-15 17:48:01 +1300

    feat: introduce isNullable type guard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export const isObject = (val: unknown): val is Record<any, any> => {
   return val !== null && typeof val === "object";
 };
+
+export const isNullable = (val: unknown): val is null | undefined => {
+  return val === null || val === undefined;
+};
