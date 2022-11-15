dde79cb892f5cb6be6ada4a4a280cbf313f14dba
Alan Soares <alanrsoars@gmail.com>
2022-11-15 20:28:43 +1300

    wip: partial wallet manager impl

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -1,4 +1,12 @@
+import { Wallet as _Wallet } from "@ethersproject/wallet";
+
 /**
  * Wallet class
  */
-export class Wallet {}
+export class Wallet {
+  #wallet?: _Wallet;
+  generate() {
+    this.#wallet = _Wallet.createRandom();
+    return this.#wallet;
+  }
+}
