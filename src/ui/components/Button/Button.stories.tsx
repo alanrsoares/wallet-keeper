82b77db5210b8ba2970d6a5eb9b84ee8183272e8
Alan Soares <alanrsoars@gmail.com>
2022-11-15 17:27:58 +1300

    feat: add Button variants

--- src/ui/components/Button/Button.stories.tsx
+++ src/ui/components/Button/Button.stories.tsx
@@ -7,7 +7,48 @@ export default {
 
 const Template = (args: Props) => <Button {...args} />;
 
-export const Primary = Template.bind({}, { children: "Primary" });
-export const Secondary = Template.bind({}, { children: "Secondary" });
-export const Large = Template.bind({}, { children: "Large", size: "lg" });
+export const Primary = Template.bind(
+  {},
+  { children: "Primary", variant: "primary" }
+);
+
+export const Secondary = Template.bind(
+  {},
+  { children: "Secondary", variant: "secondary" }
+);
+
+export const Accent = Template.bind(
+  {},
+  { children: "Accent", variant: "accent" }
+);
+
+export const Info = Template.bind({}, { children: "Info", variant: "info" });
+
+export const Warning = Template.bind(
+  {},
+  { children: "Warning", variant: "warning" }
+);
+
+export const Danger = Template.bind(
+  {},
+  { children: "Danger", variant: "danger" }
+);
+
+export const Success = Template.bind(
+  {},
+  { children: "Success", variant: "success" }
+);
+
+export const Ghost = Template.bind({}, { children: "Ghost", variant: "ghost" });
+
+export const ExtraSmall = Template.bind({}, { children: "Xs", size: "xs" });
+
 export const Small = Template.bind({}, { children: "Small", size: "sm" });
+
+export const Large = Template.bind({}, { children: "Large", size: "lg" });
+
+export const ExtraLarge = Template.bind({}, { children: "Xl", size: "xl" });
+
+export const Wide = Template.bind({}, { children: "Wide", length: "wide" });
+
+export const Block = Template.bind({}, { children: "Block", length: "block" });
--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,20 +1,66 @@
 import clsx from "clsx";
 import tw from "tailwind-styled-components";
 
-export type ButtonVariant = "primary" | "secondary" | "tertiary";
+export type ButtonVariant =
+  | "primary"
+  | "secondary"
+  | "accent"
+  | "info"
+  | "warning"
+  | "danger"
+  | "success"
+  | "ghost";
 
-export type ButtonSize = "sm" | "md" | "lg";
+export type ButtonSize = "xs" | "sm" | "md" | "lg" | "xl";
+
+export type ButtonLength = "wide" | "block";
+
+export type ButtonShape = "circle" | "square";
 
 export type Props = JSX.IntrinsicElements["button"] & {
   variant?: ButtonVariant;
+  /**
+   * @default "md"
+   */
   size?: ButtonSize;
+  /**
+   * @description "wide" is only available for "xs" and "sm" sizes
+   * @description "block" is only available for "md", "lg", and "xl" sizes
+   */
+  length?: ButtonLength;
+  shape?: ButtonShape;
 };
 
 const BaseButton = tw.button`border p-4 btn`;
 
 const Button = ({ className, ...props }: Props) => {
   return (
-    <BaseButton className={clsx(className)} {...props}>
+    <BaseButton
+      className={clsx(className, {
+        // variants
+        "btn-primary": props.variant === "primary",
+        "btn-secondary": props.variant === "secondary",
+        "btn-accent": props.variant === "accent",
+        "btn-info": props.variant === "info",
+        "btn-warning": props.variant === "warning",
+        "btn-danger": props.variant === "danger",
+        "btn-success": props.variant === "success",
+        "btn-ghost": props.variant === "ghost",
+        // sizes
+        "btn-xs": props.size === "xs",
+        "btn-sm": props.size === "sm",
+        "btn-md": props.size === "md",
+        "btn-lg": props.size === "lg",
+        "btn-xl": props.size === "xl",
+        // lengths
+        "btn-wide": props.length === "wide",
+        "btn-block": props.length === "block",
+        // shapes
+        "btn-circle": props.shape === "circle",
+        "btn-square": props.shape === "square",
+      })}
+      {...props}
+    >
       {props.children}
     </BaseButton>
   );
