c8e3ab93c9146299e578c6c5b6fb6455f80d95a4
Alan Soares <alanrsoars@gmail.com>
2022-11-16 14:59:22 +1300

    chore: fix build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,21 @@
-import React from "react";
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-
 import type { AppProps } from "next/app";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import MainLayout from "~/ui/layouts/MainLayout/MainLayout";
-
 import "~/styles/globals.css";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  const [queryClient] = React.useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <MainLayout>
       <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
+        <Hydrate state={(pageProps as any)?.dehydratedState}>
           <Component {...pageProps} />
         </Hydrate>
       </QueryClientProvider>
