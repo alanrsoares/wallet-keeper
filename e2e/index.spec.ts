a5078ada514ee7b0f97917e21837cff3ce72c217
Alan Soares <alanrsoars@gmail.com>
2022-11-21 12:56:29 +1300

    feat: run against env url

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from "@playwright/test";
 
+const BASE_URL =
+  process.env["PLAYWRIGHT_TEST_BASE_URL"] ?? "http://localhost:3000";
+
 test("homepage has title and links to intro page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto(BASE_URL);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/WalletKeeper/);
