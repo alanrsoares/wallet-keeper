fa8fc6200b87155652eb5524001fe545d05e8d19
Alan <alanrsoars@gmail.com>
2022-12-01 12:02:28 +1300

    chore: configure pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    scope: "/",
-    sw: "sw.js",
-  },
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/",
+  sw: "sw.js",
 });
 
 const nextConfig = withPWA({
