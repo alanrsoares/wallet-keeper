246af957bb95dfb09337dc3911b54c5cd4683280
Alan <alanrsoars@gmail.com>
2022-12-01 12:08:26 +1300

    chore: cleanup

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  scope: "/",
-  sw: "sw.js",
 });
 
 const nextConfig = withPWA({
   reactStrictMode: true,
-  swcMinify: true,
 });
 
 module.exports = nextConfig;
