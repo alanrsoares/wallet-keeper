0874e5ca97aa50afa50ce6640b86b66cebb6191b
Alan Soares <alanrsoars@gmail.com>
2022-12-07 12:51:22 +1300

    refactor: walletlist spec to use testing library recommended style

--- src/ui/compounds/WalletList/WalletList.spec.tsx
+++ src/ui/compounds/WalletList/WalletList.spec.tsx
@@ -1,51 +1,49 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { findByTestId, fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import { readSingleAccountFixture } from "fixtures";
 
-import {
-  WalletKeeperProvider,
-  WalletKeeperState,
-} from "~/lib/contexts/walletKeeper";
+import { WalletKeeperProvider } from "~/lib/contexts/walletKeeper";
 import WalletList, { TEST_IDS } from "./WalletList";
 
 const queryClient = new QueryClient();
 
-const renderWalletList = (initialState: WalletKeeperState) => {
-  return render(
-    <QueryClientProvider client={queryClient}>
-      <WalletKeeperProvider initialState={initialState}>
-        <WalletList />
-      </WalletKeeperProvider>
-    </QueryClientProvider>
-  );
-};
-
 describe("WalletList", () => {
   describe("Single wallet", async () => {
     const initialState = await readSingleAccountFixture();
-    const { container } = renderWalletList(initialState);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <WalletKeeperProvider initialState={initialState}>
+          <WalletList />
+        </WalletKeeperProvider>
+      </QueryClientProvider>
+    );
 
     it("should render the correct wallet count", async () => {
-      const walletCount = await findByTestId(container, TEST_IDS.walletCount);
+      const walletCount = await screen.findByTestId(TEST_IDS.walletCount);
       expect(walletCount.textContent).toBe("(1)");
     });
 
     it("should be expanded by default", async () => {
-      const walletList = await findByTestId(container, TEST_IDS.walletList);
+      const walletList = await screen.findByTestId(TEST_IDS.walletList);
 
-      expect(walletList).toBeTruthy();
+      expect(walletList).toBeVisible();
     });
 
     it("should collapse the list when the toggle is clicked", async () => {
-      const toggle = await findByTestId(container, TEST_IDS.toggleButton);
+      const toggle = await screen.findByTestId(TEST_IDS.toggleButton);
 
-      expect(toggle.classList.contains("swap-active")).toBe(true);
+      expect(toggle).toHaveClass("swap-active");
 
       fireEvent.click(toggle);
 
-      expect(toggle.classList.contains("swap-active")).toBe(false);
+      const walletList = screen.queryByTestId(TEST_IDS.walletList);
+
+      expect(toggle).not.toHaveClass("swap-active");
+
+      expect(walletList).not.toBeTruthy();
     });
   });
 });
