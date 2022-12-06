28ca13359a96ee4cbec2b725dd24b6cbcdec2576
Alan Soares <alanrsoars@gmail.com>
2022-12-06 16:14:51 +1300

    ui: sticky footer tweaks

--- src/ui/layouts/MainLayout/_components/Footer.tsx
+++ src/ui/layouts/MainLayout/_components/Footer.tsx
@@ -1,13 +1,23 @@
+import clsx from "clsx";
 import { APP_NAME } from "~/lib/constants";
+import useIsSticky from "~/lib/hooks/useIsSticky";
 import useVersion from "~/lib/hooks/useVersion";
 
 const Footer = () => {
   const version = useVersion();
+  const isSticky = useIsSticky(32);
 
   return (
-    <footer className="bg-base-300 p-3 lg:p-4">
-      <div className="container-narrow max-auto grid place-items-center font-mono">
-        v{version} &copy; {new Date().getFullYear()} {APP_NAME}
+    <footer
+      className={clsx("bg-base-300 p-3 lg:p-4 sticky bottom-0 group", {
+        "drop-shadow-md": isSticky,
+      })}
+    >
+      <div className="container-narrow max-auto flex gap-1 items-center justify-center font-mono">
+        v{version ?? "next"} &copy; {new Date().getFullYear()}{" "}
+        <span className="font-display transition-colors group-hover:text-cyan-200">
+          {APP_NAME}
+        </span>
       </div>
     </footer>
   );
