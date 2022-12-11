9ccd9c1f58e95c260bad1cbcb3ef9ba76eb2e286
Alan <alanrsoars@gmail.com>
2022-12-11 23:07:40 +1300

    chore: tweak mobile design

--- src/ui/components/Dropdown/Dropdown.tsx
+++ src/ui/components/Dropdown/Dropdown.tsx
@@ -3,14 +3,16 @@ import clsx from "clsx";
 
 export type DropdownProps<T extends string> = {
   value: string;
-  options: { value: T; label: string; url: string }[];
+  options: ReadonlyArray<{ value: T; label: string; url: string }>;
   className?: string;
+  onChange?: (value: T) => void;
 };
 
 export const Dropdown = <T extends string>({
   value,
   options,
   className,
+  onChange,
 }: DropdownProps<T>) => {
   const selectedOption = options.find((option) => option.value === value);
 
@@ -24,18 +26,24 @@ export const Dropdown = <T extends string>({
       </label>
       <ul
         tabIndex={0}
-        className="dropdown-content menu p-2 shadow bg-base-300 rounded-box w-40"
+        className="dropdown-content menu p-2 shadow bg-base-300 rounded-box w-40 grid gap-1"
       >
         {options.map((option) => (
           <li key={option.value}>
-            <a role="button" href={option.url} className="flex justify-between">
+            <button
+              role="option"
+              className="flex justify-between"
+              onClick={() => {
+                onChange?.(option.value);
+              }}
+            >
               {option.label}
               {value === option.value && (
                 <span className="text-success">
                   <CheckIcon className="h-4 w-4" />
                 </span>
               )}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
--- src/ui/compounds/EnvSwitcher/EnvSwitcher.tsx
+++ src/ui/compounds/EnvSwitcher/EnvSwitcher.tsx
@@ -1,7 +1,7 @@
-import { CheckIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { useRouter } from "next/router";
 import { FC, useEffect } from "react";
+
 import { useWalletKeeper } from "~/lib/contexts/walletKeeper";
 import { Dropdown } from "~/ui/components";
 
@@ -10,9 +10,9 @@ export type EnvSwitcherProps = {};
 const ENV_OPTIONS = [
   { value: "mainnet", label: "Mainnet", url: "/?env=mainnet" },
   { value: "testnet", label: "Testnet", url: "/?env=testnet" },
-];
+] as const;
 
-export const EnvSwitcher: FC<EnvSwitcherProps> = (props) => {
+export const EnvSwitcher: FC<EnvSwitcherProps> = (_props) => {
   const { state, actions } = useWalletKeeper();
   const networkEnv = useRouter().query.env as string;
 
@@ -33,6 +33,9 @@ export const EnvSwitcher: FC<EnvSwitcherProps> = (props) => {
           "bg-primary": network === "mainnet",
           "bg-secondary": network === "testnet",
         })}
+        onChange={(value) => {
+          actions.selectNetwork(value === "mainnet" ? "homestead" : "goerli");
+        }}
       />
     </div>
   );
