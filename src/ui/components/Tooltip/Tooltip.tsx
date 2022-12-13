54823e7705eac7fc96a2284c159a5df4892174d3
Alan Soares <alanrsoars@gmail.com>
2022-12-13 19:32:55 +1300

    feat: add variance and stories for Tooltip

--- src/ui/components/Tooltip/Tooltip.tsx
+++ src/ui/components/Tooltip/Tooltip.tsx
@@ -1,8 +1,34 @@
-import clsx from "clsx";
+import { cva, VariantProps } from "class-variance-authority";
 import { FC } from "react";
+
 import { TestableProps } from "~/lib/test-utils";
 
+const variance = cva("tooltip", {
+  variants: {
+    position: {
+      top: "tooltip-top",
+      right: "tooltip-right",
+      bottom: "tooltip-bottom",
+      left: "tooltip-left",
+    },
+    variant: {
+      primary: "tooltip-primary",
+      secondary: "tooltip-secondary",
+      accent: "tooltip-accent",
+      info: "tooltip-info",
+      warning: "tooltip-warning",
+      danger: "tooltip-error",
+    },
+    open: {
+      true: "tooltip-open",
+    },
+  },
+});
+
+type VProps = VariantProps<typeof variance>;
+
 export type TooltipProps = JSX.IntrinsicElements["div"] &
+  VProps &
   TestableProps<{
     tip: string;
   }>;
@@ -11,11 +37,14 @@ export const Tooltip: FC<TooltipProps> = ({
   tip,
   className,
   children,
+  position,
+  variant,
+  open,
   ...props
 }) => (
   <div
     {...props}
-    className={clsx("tooltip", className)}
+    className={variance({ className, position, variant, open })}
     data-tip={tip}
     data-testid={props.testId || props["data-testid"]}
   >
